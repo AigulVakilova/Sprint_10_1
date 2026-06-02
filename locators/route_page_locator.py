328883ff108d1eaa2b00ebc82c1b7d25a311460c
AigulVakilova <vakilowaa@yandex.ru>
2026-06-02 12:02:08 +0400

    Fix CALL_TAXI_BUTTON and BOOK_DRIVE_BUTTON locators

--- locators/route_page_locator.py
+++ locators/route_page_locator.py
@@ -10,5 +10,5 @@ class RoutePageLocators:
     ROUTE_TIME = (By.XPATH, "//div[@class='results-text']//div[@class='duration']")
     TRANSPORT_ALL = (By.XPATH, "//div[@class='types-container']/div")
     TRANSPORT_DRIVE = (By.XPATH, "//div[@class='types-container']//div[@class='type drive']")
-    CALL_TAXI_BUTTON = (By.XPATH, "//div[@class='results-text']//button[@class='button round']")
-    BOOK_DRIVE_BUTTON = (By.XPATH, "//div[@class='results-text']//button[@class='button round']")
+    CALL_TAXI_BUTTON = (By.XPATH, "//div[@class='results-text']//button[@class='button round' and text()='Вызвать такси']")
+    BOOK_DRIVE_BUTTON = (By.XPATH, "//div[@class='results-text']//button[@class='button round' and text()='Забронировать']")
